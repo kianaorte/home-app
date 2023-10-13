78eb8dbc0558b6466c8fe0485e71d0ae08cfaeb6
kiana.ortega <kiana.ortega@fusionnetworks.co.nz>
2023-10-14 03:00:27 +1100

    click through janky

--- src/pages/Scene1.tsx
+++ src/pages/Scene1.tsx
@@ -5,7 +5,7 @@ const welcomeDialogue = [{
     text: 'Happy birthday my sweetie baby!'
 },
 {
-    text: 'I love you so so much'
+    text: 'I love you sooo so so soo so sooo so so so so so so  much'
 
 },
 {
@@ -15,6 +15,15 @@ const welcomeDialogue = [{
 {
     text: 'I made you a small interactive game!'
 
+},
+{
+    text: "I drew all the art and wrote all the code..."
+},
+{
+    text: 'Even the dialogue you are reading now was handwritten by me!'
+},
+{
+    text: 'Everything was made from scratch so if its janky I apologize lololol'
 }
 
 ]
@@ -24,9 +33,16 @@ export const Scene1 = () => {
     const removeElement = () => {
         setVisible(false);
     };
+
+    function removeFunction() {
+        if (welcomeDialogue.length > 1 && visible === false) {
+            welcomeDialogue.shift();
+            console.log('inside: ' + welcomeDialogue[0].text);
+            setVisible(true);
+        }
+    }
     useEffect(() => {
-        welcomeDialogue.shift();
-        console.log('dialogue: ' + welcomeDialogue[0].text);
+        window.addEventListener("click", removeFunction);
     }, [visible]);
     return (
         <div onClick={removeElement}>
