3a95121f2e06cfa94e3e0e11ffbe37e4fb15dd26
kiana.ortega <kiana.ortega@fusionnetworks.co.nz>
2023-10-19 22:56:23 +1100

    wonky hover over key items

--- src/components/BRoom.tsx
+++ src/components/BRoom.tsx
@@ -1,15 +1,18 @@
 import './Bedroom.scss'
 
-import gamingPc from '../assets/images/backgrounds/bedroom/GamingPc.png';
+import door from '../assets/images/backgrounds/bedroom/door.png';
 import bed from '../assets/images/backgrounds/bedroom/Bed.png';
 import photoboard from '../assets/images/backgrounds/bedroom/PhotoBoard.png';
+
+import pc from '../assets/images/backgrounds/bedroom/pc.png';
 
 export const BRoom = () => {
     return (
         <div className='bedroom-container'>
-            <img src={gamingPc} alt='setup' width='100%' className='bedroom-items' />
-            <img src={bed} alt='bed' width='100%' className='bedroom-items' />
-            <img src={photoboard} alt='photos' width='100%' />
+            <img src={door} alt='door' width='12%' className='door' />
+            <img src={bed} alt='bed' width='40%' className='bed' />
+            <img src={photoboard} alt='photos' width='12%' className='photoboard' />
+            <img src={pc} alt='setup' width='18%' className='gaming-setup' />
         </div>
     )
 };
