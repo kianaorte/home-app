d44190751bc3209cb62179dcca902c4ce1dbe46a
kiana.ortega <kiana.ortega@fusionnetworks.co.nz>
2023-10-20 21:43:26 +1100

    item overlay

--- src/components/ItemOverlay.tsx
+++ src/components/ItemOverlay.tsx
@@ -1,19 +1,28 @@
 import './ItemOverlay.scss';
 import popUpContainer from '../assets/images/others/PopUpContainer.png';
 import BlueBtn from '../assets/images/others/BlueBtn.png'
+import { Dispatch, SetStateAction } from 'react';
 
-export const ItemOverlay = (props: any) => {
+export const ItemOverlay = (props: { item: string, description: string[], sendToParent: Dispatch<SetStateAction<boolean>> }) => {
     const { sendToParent } = props;
+    const itemName = props.item;
+    const description = props.description;
 
     const closeOverlay = () => {
         sendToParent(false);
         console.log('close overlay: ' + sendToParent);
     }
+    console.log('itemname: ' + itemName);
+    console.log('desc: ' + description);
 
     return (
         <div className='overlay-container'>
-            <img src={popUpContainer} alt="popUp" className='popup' />
-            <h1 className='subtitle'>Item name goes here</h1>
+            <div className='yeehaw'>
+                <img src={popUpContainer} alt="popUp" className='popup' />
+                <h1 className='subtitle'>{itemName}</h1>
+                <p className='para'> {description}</p>
+            </div>
+
             <div className='bluebtn-close' onClick={closeOverlay}>
                 <img src={BlueBtn} alt='btn' width='80%' />
                 <h1 className='close-text'>Close</h1>
--- src/pages/Scene3.tsx
+++ src/pages/Scene3.tsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 import { ItemOverlay } from "../components/ItemOverlay";
 
+const practiceItem = [{
+    item: 'Board games',
+    description: ["One of my favourite passtimes is spending time and playing games with you", "I know we don't have all these games but I'm looking forward to collecting and learning more games with you",]
+}]
+
 
 const Scene3 = () => {
 
     const [overlay, setShowOverlay] = useState(true);
     return (
         <div>
-            {overlay && <ItemOverlay sendToParent={setShowOverlay} />}
+            {/* so id need to map over the item and then map over the description in the overlay? */}
+            {overlay && <ItemOverlay item={practiceItem[0].item} description={practiceItem[0].description} sendToParent={setShowOverlay} />}
         </div>
     );
 }
